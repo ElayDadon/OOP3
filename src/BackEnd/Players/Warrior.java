9d6a7755fc87ae862c0107d61fc1c5629769a48f
amit-levints-bgu <levints@post.bgu.ac.il>
2022-06-26 18:14:01 +0300

    warrior

--- src/BackEnd/Players/Warrior.java
+++ src/BackEnd/Players/Warrior.java
@@ -2,6 +2,7 @@ package BackEnd.Players;
 
 import BackEnd.Enemys.Enemy;
 import java.util.List;
+import java.util.Random;
 
 public class Warrior extends Player{
     public static final int MAX_RANGE = 3;
@@ -30,11 +31,13 @@ public class Warrior extends Player{
 
     public void abilityCasting(List<Enemy> enemies){ // Avenger's Shield
         if(remainingCooldown == 0){
-            remainingCooldown = abilityCooldown;
+            remainingCooldown = abilityCooldown; // After casting ability, should wait again
             super.HealthAmount = Math.min(super.HealthAmount + 10* super.DefensePoints, super.HealthPool);
             List<Enemy> inRange = this.filterRange(enemies, MAX_RANGE);
             if(!inRange.isEmpty()){
-
+                Random rand = new Random();
+                Enemy e = inRange.get(rand.nextInt(inRange.size()));
+                e..HealthAmount =  e.HealthAmount - super.HealthPool/10;
             }
         } else {
             tickingGame();
