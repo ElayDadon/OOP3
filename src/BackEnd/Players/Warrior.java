aacd40206be086a5bd05a24f68e22da191e1f13c
amit-levints-bgu <levints@post.bgu.ac.il>
2022-06-26 17:03:49 +0300

    ability casting

--- src/BackEnd/Players/Warrior.java
+++ src/BackEnd/Players/Warrior.java
@@ -4,6 +4,7 @@ import BackEnd.Enemys.Enemy;
 import java.util.List;
 
 public class Warrior extends Player{
+    public static final int MAX_RANGE = 3;
     Integer abilityCooldown; // number of game ticks required to pass before using the ability.
     Integer remainingCooldown;  //number of ticks remained until the warrior can use its special ability.
 
@@ -24,14 +25,19 @@ public class Warrior extends Player{
     }
 
     public void tickingGame(){
-        this.remainingCooldown = this.remainingCooldown -1;
+        this.remainingCooldown = Math.max(this.remainingCooldown -1, 0);
     }
 
-    public void abilityCasting(Player player, List<Enemy> enemies){ // Avenger's Shield
-        if(super.HealthAmount == 0){
+    public void abilityCasting(List<Enemy> enemies){ // Avenger's Shield
+        if(remainingCooldown == 0){
+            remainingCooldown = abilityCooldown;
+            super.HealthAmount = Math.min(super.HealthAmount + 10* super.DefensePoints, super.HealthPool);
+            List<Enemy> inRange = this.filterRange(enemies, MAX_RANGE);
+            if(!inRange.isEmpty()){
 
+            }
+        } else {
+            tickingGame();
         }
-        remainingCooldown = abilityCooldown;
-        super.HealthAmount = Math.min(super.HealthAmount + 10* super.DefensePoints, super.HealthPool);
     }
 }
