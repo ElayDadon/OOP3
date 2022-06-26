e3dd299258d8cd2d7aa2cb97f1c97220906e22df
amit-levints-bgu <levints@post.bgu.ac.il>
2022-06-26 11:21:43 +0300

    warrior

--- src/BackEnd/Players/Player.java
+++ src/BackEnd/Players/Player.java
@@ -7,7 +7,6 @@ public class Player extends Unit {
     protected int PlayerLevel; // Increased When Experience reached to 50 X level
 
 
-
     public Player( int HealthPool, int HealthAmount, char tile)
     {
         super(tile);
@@ -15,15 +14,15 @@ public class Player extends Unit {
         this.PlayerLevel = 1;
     }
 
-//    public void LevelingUp()
-//    {
-//        if (PlayerLevel * 50 <= Experience){
-//            Experience =- PlayerLevel*50;
-//            PlayerLevel ++;
-//            HealthPool =+ 10 * PlayerLevel;
-//            HealthAmount = HealthPool;
-//            AttackPoints =+4 * PlayerLevel;
-//            DefensePoints =+ PlayerLevel;
-//        }
-//    }
+    public void LevelingUp()
+    {
+        if (PlayerLevel * 50 <= Experience){
+            Experience =- PlayerLevel*50;
+            PlayerLevel ++;
+            HealthPool =+ 10 * PlayerLevel;
+            HealthAmount = HealthPool;
+            AttackPoints =+4 * PlayerLevel;
+            DefensePoints =+ PlayerLevel;
+        }
+    }
 }
--- src/BackEnd/Players/Warrior.java
+++ src/BackEnd/Players/Warrior.java
@@ -4,15 +4,28 @@ public class Warrior extends Player{
     Integer abilityCooldown; // number of game ticks required to pass before using the ability.
     Integer remainingCooldown;  //number of ticks remained until the warrior can use its special ability.
 
-    public Warrior(int abilityCooldown, char tile)
+    public Warrior(int abilityCooldown, char remainingCooldown)
     {
-        super(tile);
+        super();
         this.abilityCooldown = abilityCooldown;
         this.remainingCooldown = 0;
     }
 
     public void levelingUpWarrior()
     {
+        super.LevelingUp();
+        this.remainingCooldown = 0;
+        super.HealthPool =+ 5* super.HealthPool;
+        super.AttackPoints =+ 2* super.PlayerLevel;
+        super.DefensePoints =+ super.DefensePoints;
+    }
 
+    public void tickingGame(){
+        this.remainingCooldown = this.remainingCooldown -1;
+    }
+
+    public void abilityCasting(){ // Avenger's Shield
+        remainingCooldown = abilityCooldown;
+        super.HealthAmount = Math.min(super.HealthAmount + 10* super.DefensePoints, super.HealthPool);
     }
 }
