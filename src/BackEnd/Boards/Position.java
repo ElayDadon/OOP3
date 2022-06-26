971c577bc170a1de82dbe58c2bd2999f5101d9d0
elaydadon <elaydad@gmail.com>
2022-06-26 20:03:38 +0300

    This is the changes for now, check and pull from main

--- src/BackEnd/Boards/Position.java
+++ src/BackEnd/Boards/Position.java
@@ -3,7 +3,14 @@ package BackEnd.Boards;
 public class Position implements Comparable<Position>{
     private int x;
     private int y;
-
+public Position(int x,int y){
+    this.x = x;
+    this.y = y;
+}
+public void set(int x, int y){
+    this.x = x;
+    this.y = y;
+}
     @Override
     public int compareTo(Position position) {
         if(x == position.getX() && y == position.getY()){
