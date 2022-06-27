56623d6ef58b8611b7921e95f65cb9e1e79d2986
amit-levints-bgu <levints@post.bgu.ac.il>
2022-06-27 21:02:19 +0300

    java package java does not exist

--- src/FrontEnd/FileHandler/levelMaker.java
+++ src/FrontEnd/FileHandler/levelMaker.java
@@ -1,5 +1,4 @@
 package FrontEnd.FileHandler;
-import java.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
--- src/FrontEnd/GameHandler/Input.java
+++ src/FrontEnd/GameHandler/Input.java
@@ -1,5 +1,4 @@
 package FrontEnd.GameHandler;
-import java.*;
 import java.util.Scanner;
 
 public class Input {
