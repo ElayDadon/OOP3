86332c1d7bf18c463a9f31af65ea72021a8b7fbc
orsaada <saadao@post.bgu.ac.il>
2022-06-30 15:33:38 +0300

    Mage test

--- src/PresentationLayer/Game.java
+++ src/PresentationLayer/Game.java
@@ -7,12 +7,7 @@ import BackEnd.Enemys.Monster;
 public class Game {
     public static void main(String[] args) {
 //        String path = "C:/Users/saada/Desktop/OrStudys/OOP_game/levels_dir";
-        if(args.length > 1){
-            GameManager gameManager = new GameManager(args[0]);
-            gameManager.start();
-        }
-        else {
-            throw new RuntimeException("missing levels path");
-        }
+        GameManager gameManager = new GameManager(args[0]);
+        gameManager.start();
     }
 }
